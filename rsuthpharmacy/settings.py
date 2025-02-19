65b1c39c26822942a299dc1dcef8a75806e5c9a9
chistev <stephenowabie@gmail.com>
2025-02-19 12:05:11 +0100

    not hardcoding secret key

--- rsuthpharmacy/settings.py
+++ rsuthpharmacy/settings.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m35byu5%@mdk#ugie^ik%fq)7&hjpztkc_oh-#tkm0l^$3dvy5'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("Missing DJANGO_SECRET_KEY in environment variables")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
