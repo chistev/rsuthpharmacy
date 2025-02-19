973b416ce07db2da01c14435549fe7ce79c3d12e
chistev <stephenowabie@gmail.com>
2025-02-19 08:22:33 +0100

    rendering products under selected categories

--- price_list/urls.py
+++ price_list/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<slug:category_slug>/', views.index, name='category_detail'),
 ]
--- price_list/views.py
+++ price_list/views.py
@@ -1,8 +1,8 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from price_list.models import Category, Product
 
-def index(request):
+def index(request, category_slug=None):
     categories = list(Category.objects.all())
 
     # Move "Others" category to the end
@@ -11,12 +11,15 @@ def index(request):
         categories.remove(others_category)
         categories.append(others_category)
 
-    selected_category_slug = request.GET.get('category', None)
+    selected_category_slug = category_slug or request.GET.get('category', None)
 
+    # Fetch the selected category or default to "Tablets/Capsules" category
+    selected_category = None
     if selected_category_slug:
-        category = Category.objects.get(slug=selected_category_slug)
-        products = Product.objects.filter(category=category).order_by("brand_name", "generic_name")
+        selected_category = get_object_or_404(Category, slug=selected_category_slug)
+        products = Product.objects.filter(category=selected_category).order_by("brand_name", "generic_name")
     else:
+        # If no category is selected, show all products
         products = Product.objects.all().order_by("brand_name", "generic_name")
 
     # Pagination: Show 40 products per page
@@ -26,5 +29,6 @@ def index(request):
 
     return render(request, 'price_list/index.html', {
         'categories': categories,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'selected_category': selected_category,
     })
